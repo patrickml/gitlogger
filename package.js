fdc161e75a6da7e258466c4033db755dba7497b3
Patrick Lewis <Patrickml.pml@gmail.com>
2015-12-17 16:44:50 -0500

    Changed Package Load Order

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'patrickml:git-logger',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Sends your Client and Server errors to Githubs issue tracker',
   // URL to the Git repository containing the source code for this package.
@@ -36,8 +36,8 @@ Package.onUse(function(api) {
   ]);
 
   api.addFiles([
-    'lib/server/error-tracker.js',
     'lib/server/github.js',
+    'lib/server/error-tracker.js',
     'lib/server/methods.js'
   ],[
     'server'
